f2ea2dd69cd778717a252c88bbf93ba37c72942e
Shadowar07 <loic.verilhac7@gmail.com>
2017-04-03 19:10:53 +0200

    Version 1.0

--- web/troc/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TrocConfig(AppConfig):
-    name = 'troc'
--- web/troc/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class RegisterForm(forms.Form):
-    login = forms.CharField(max_length=10)
-    passwd = forms.CharField(max_length=10)
-
